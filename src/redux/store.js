70d5d019d4b45462b89d8b707ceb31673c54cef6
unknown <ahmedesmaill1431999@gmail.com>
2020-05-20 07:08:53 +0200

    magdy make changes on production and he is a good man

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import { persistStore } from "redux-persist";
 import { logger } from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [];
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 
 export const Store = createStore(rootReducer, applyMiddleware(...middleWares));
 
